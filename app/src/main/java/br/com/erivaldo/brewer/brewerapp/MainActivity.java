38773c274e0633b81c258bddb73dabea1d1bf897
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-03-10 00:19:40 -0300

    First Commit

---
+++ app/src/main/java/br/com/erivaldo/brewer/brewerapp/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.erivaldo.brewer.brewerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index);
+        ImageView imagem = (ImageView) findViewById(R.id.homer);
+        imagem.setImageResource(R.drawable.homer);
+    }
+}
