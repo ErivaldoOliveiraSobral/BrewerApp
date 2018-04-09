25658b401f1a5d7c092fcc26838cda67174ff761
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-04-09 19:53:12 -0300

    SimpleAdapter

---
+++ app/src/main/java/br/com/erivaldo/brewer/brewerapp/SimpleAdapter.java
@@ -0,0 +1,31 @@
+package br.com.erivaldo.brewer.brewerapp;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Android on 09/04/2018.
+ */
+
+public class SimpleAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
